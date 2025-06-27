49e19c2117959dca0bda3cf61d323ff1647a0e39
Rchit13 <2008architgupta@gmail.com>
2025-06-27 17:26:55 +0530

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MentoroApp from './MentoroApp';
+import './index.css'; // optional, if you have it
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MentoroApp />
+  </React.StrictMode>
+);
