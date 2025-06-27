5b5037927a9f43e4a8b9b883fe6808dd07af7ac1
Rchit13 <2008architgupta@gmail.com>
2025-06-27 17:31:37 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MentoroApp from './MentoroApp';
-import './index.css'; // optional, if you have it
+import ClassDetailPage from './ClassDetailPage';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MentoroApp />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MentoroApp />} />
+        <Route path="/class/:id" element={<ClassDetailPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
